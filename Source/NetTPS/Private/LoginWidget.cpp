4a8f0165f79b59c5fb3b13cdd3ed76fd959f138f
DESKTOP-A2AQS7S\MSI <technoisekor@gmail.com>
2025-04-14 14:51:09 +0900

    OnlineSession 제작 Lobby UI 제작

---
+++ Source/NetTPS/Private/LoginWidget.cpp
@@ -0,0 +1,35 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "LoginWidget.h"
+#include "NetGameInstance.h"
+#include "Components/Button.h"
+#include "Components/Slider.h"
+#include "Components/TextBlock.h"
+#include "Components/EditableText.h"
+
+void ULoginWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	gi = Cast<UNetGameInstance>(GetWorld()->GetGameInstance());
+	btn_createRoom->OnClicked.AddDynamic(this, &ULoginWidget::CreateRoom);	
+	slider_playerCount->OnValueChanged.AddDynamic(this, &ULoginWidget::OnValueChanged);
+
+
+}
+
+void ULoginWidget::CreateRoom()
+{
+	if( gi && edit_roomName->GetText().IsEmpty() == false )
+	{
+		FString roomName = edit_roomName->GetText().ToString();
+		int32 playerCount = slider_playerCount->GetValue();
+		gi->CreateMySession(roomName, playerCount);
+	}
+}
+
+void ULoginWidget::OnValueChanged(float Value)
+{
+	txt_playerCount->SetText(FText::AsNumber(Value));	
+}
---
+++ Source/NetTPS/Public/LoginWidget.h
@@ -0,0 +1,44 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "LoginWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NETTPS_API ULoginWidget : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, meta=(BindWidget))
+	class UButton* btn_createRoom;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, meta=(BindWidget))
+	class UEditableText*	edit_roomName;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, meta=(BindWidget))
+	class USlider* slider_playerCount;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, meta=(BindWidget))
+	class UTextBlock* txt_playerCount;
+
+	UPROPERTY()
+	class UNetGameInstance* gi;
+
+public:
+	virtual void NativeConstruct()	override;
+
+
+	UFUNCTION()
+	void CreateRoom();
+
+	// Slider callback
+	UFUNCTION()
+	void OnValueChanged(float Value);
+	
+};
